4af60b072688d9e2b52d27fb8fb37e4a74cf3f18
BatFace <echambers0@gmail.com>
2015-09-17 02:02:12 +0200

    Adding SVG scaling to the game area.

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 requirejs.config({
     paths: {
-        jquery: 'lib/jquery/dist/jquery.min',
-        flipClock: 'lib/FlipClock/compiled/flipclock.min'
-    },
-    shim:{
-        'flipClock':{
-            deps: ['jquery']
-        }
+        jquery: 'lib/jquery/dist/jquery.min'
     }
 });
 
--- js/sticksGame.js
+++ js/sticksGame.js
@@ -41,13 +41,13 @@ define(['../js/timer.js', 'jquery', '../lib/d3/d3.min.js'], function (timer, $) 
             if (!$('#levelNumericControl')[0].checkValidity()) {
                 disablePlayPauseButton();
 
-                // Trick to make the browser display native HTML5 error
+                // Force the browser display native HTML5 error
                 // for the selected level number
                 $('#gameLevelForm').find(':submit').click();
             }
             else {
                 enablePlayPauseButton();
-                refresh();
+                redraw();
             }
         }
 
@@ -57,30 +57,41 @@ define(['../js/timer.js', 'jquery', '../lib/d3/d3.min.js'], function (timer, $) 
             $('#pauseScreen').toggle(true);
         }
 
-        function refresh() {
-            d3.select("svg").remove();
-
-            gameLevel = $('#levelNumericControl').val();
-            var finalStickIndex = gameLevel - 1;
-            var dataSet = [];
+        function calculateGameArea(){
 
             var gameAreaBounds = $('#gameArea')[0].getBoundingClientRect();
-
             var gameAreaMiddle = [(gameAreaBounds.right - gameAreaBounds.left) / 2,
                 (gameAreaBounds.bottom - gameAreaBounds.top) / 2];
-
             var stickLength = gameAreaBounds.height / 3.5;
-
             var proposedStickWidth = stickLength / 16;
             var stickWidth = proposedStickWidth > 12 ? proposedStickWidth : 12;
-
             var strokeLength = stickLength + stickWidth;
             var strokeDashArray = strokeLength + ","
                 + strokeLength;
             var strokeWidth = stickWidth / 5;
 
+            return {
+                gameAreaBounds: gameAreaBounds,
+                gameAreaMiddle: gameAreaMiddle,
+                stickLength: stickLength,
+                stickWidth: stickWidth,
+                strokeDashArray: strokeDashArray,
+                strokeWidth: strokeWidth
+            };
+        }
+
+        function redraw() {
+            d3.select("svg").remove();
+
+            var dataSet = [];
+
+            gameLevel = $('#levelNumericControl').val();
+            var finalStickIndex = gameLevel - 1;
+
+            var gameArea = calculateGameArea();
+
             for (var i = 0; i < gameLevel; i++) {
-                var newStick, oldStick
+                var newStick, oldStick;
                 var s = getRandomNumberInRange(lowCrossingLimit, highCrossingLimit);
                 var t = getRandomNumberInRange(lowCrossingLimit, highCrossingLimit);
 
@@ -91,24 +102,26 @@ define(['../js/timer.js', 'jquery', '../lib/d3/d3.min.js'], function (timer, $) 
                 else {
                     var rotation = getRandomNumberInRange(fiveDeg, oneSevenFiveDeg);
 
-                    // To place first stick, first calculating the position of a dummy old stick
-                    // in the centre of the game area in order to lessen chance of t being quickly out
+                    // To place first stick, first calculate the position of a dummy old stick
+                    // in the centre of the game area in order to lessen chance of it being quickly out
                     // of range for subsequent sticks
-                    var x = gameAreaMiddle[0] + stickLength / 2;
-                    var y = gameAreaMiddle[1];
+                    var x = gameArea.gameAreaMiddle[0] + gameArea.stickLength / 2;
+                    var y = gameArea.gameAreaMiddle[1];
 
                     oldStick = new sticky(0.5, 0.5, ninetyDeg, stickColors[getRandomIntInRange(0, 4)], x, y);
                 }
 
                 newStick = generateNewStickXY(oldStick,
-                                              new sticky(s, t, rotation, stickColors[getRandomIntInRange(0, 4)]));
+                    new sticky(s, t, rotation, stickColors[getRandomIntInRange(0, 4)]));
                 dataSet.push(newStick);
             }
 
             var svg = d3.select("#gameArea")
+                .classed("svg-container", true)
                 .append("svg")
-                .attr("width", gameAreaBounds.width)
-                .attr("height", gameAreaBounds.height);
+                .classed("svg-content-responsive", true)
+                .attr("preserveAspectRatio", "none")
+                .attr("viewBox", "0 0 " + gameArea.gameAreaBounds.width + " " + gameArea.gameAreaBounds.height);
 
             update();
 
@@ -137,37 +150,44 @@ define(['../js/timer.js', 'jquery', '../lib/d3/d3.min.js'], function (timer, $) 
                 rectangles.attr("x", function (d, i) {
                     return d.x;
                 })
-                    .attr("y", function (d, i) {
-                        return d.y;
-                    })
-                    .attr("width", stickWidth)
-                    .attr("height", stickLength)
-                    .attr("transform", function (d) {
-                        return 'rotate('
-                            + (d.rotation * (180 / Math.PI)) + ' '
-                            + d.x + ' ' + d.y + ')';
-                    })
-                    .attr("stroke", "black")
-                    .attr("stroke-width", strokeWidth)
-                    .attr("stroke-dasharray", strokeDashArray)
-                    .attr("fill", function (d) {
-                        return d.color;
-                    })
-                    .on("click", function (d, i) {
-                        removeIfLast(i);
-                        update();
-                    });
+                .attr("y", function (d, i) {
+                    return d.y;
+                })
+                .attr("width", gameArea.stickWidth)
+                .attr("height", gameArea.stickLength)
+                .attr("transform", function (d) {
+                    return 'rotate('
+                        + (d.rotation * (180 / Math.PI)) + ' '
+                        + d.x + ' ' + d.y + ')';
+                })
+                .attr("stroke", "black")
+                .attr("stroke-width", gameArea.strokeWidth)
+                .attr("stroke-dasharray", gameArea.strokeDashArray)
+                .attr("fill", function (d) {
+                    return d.color;
+                })
+                .on("click", function (d, i) {
+                    removeIfLast(i);
+                });
             }
 
             function removeIfLast(index) {
                 if (index === finalStickIndex) {
-                    dataSet.pop();
-                    finalStickIndex--;
-
-                    if (finalStickIndex === -1) {
+                    if (finalStickIndex === 0) {
                         winGame();
                     }
+                    else {
+                        removeStick();
+                    }
                 }
+            }
+
+            function removeStick() {
+                dataSet.pop();
+                finalStickIndex--;
+                var rectangles = svg.selectAll("rect")
+                    .data(dataSet);
+                rectangles.exit().remove();
             }
 
             function getRandomIntInRange(min, max) {
@@ -200,34 +220,34 @@ define(['../js/timer.js', 'jquery', '../lib/d3/d3.min.js'], function (timer, $) 
                 var aOld, bOld, aNew, bNew;
 
                 if (isInRange(oldStick.rotation, fiveDeg, ninetyDeg)) {
-                    aOld = -(stickLength * Math.sin(oldStick.rotation));
-                    bOld = (stickLength * Math.cos(oldStick.rotation));
+                    aOld = -(gameArea.stickLength * Math.sin(oldStick.rotation));
+                    bOld = (gameArea.stickLength * Math.cos(oldStick.rotation));
                 }
                 else // ninetyDeg <= oldStick.rotation <= oneSevenFiveDeg
                 {
                     var oldRotationMinusNinetyDeg = oldStick.rotation - ninetyDeg;
 
-                    aOld = -(stickLength * Math.cos(oldRotationMinusNinetyDeg));
-                    bOld = -(stickLength * Math.sin(oldRotationMinusNinetyDeg));
+                    aOld = -(gameArea.stickLength * Math.cos(oldRotationMinusNinetyDeg));
+                    bOld = -(gameArea.stickLength * Math.sin(oldRotationMinusNinetyDeg));
                 }
 
                 if (isInRange(newStick.rotation, fiveDeg, ninetyDeg)) {
-                    aNew = stickLength * Math.sin(newStick.rotation);
-                    bNew = -stickLength * Math.cos(newStick.rotation);
+                    aNew = gameArea.stickLength * Math.sin(newStick.rotation);
+                    bNew = -gameArea.stickLength * Math.cos(newStick.rotation);
                 }
                 else // ninetyDeg <= newStick.rotation <= oneSevenFiveDeg
                 {
                     var newRotationMinusNinetyDeg = newStick.rotation - ninetyDeg;
 
-                    aNew = stickLength * Math.cos(newRotationMinusNinetyDeg);
-                    bNew = stickLength * Math.sin(newRotationMinusNinetyDeg);
+                    aNew = gameArea.stickLength * Math.cos(newRotationMinusNinetyDeg);
+                    bNew = gameArea.stickLength * Math.sin(newRotationMinusNinetyDeg);
                 }
 
                 newStick.x = oldStick.x + oldStick.propAlongStickLengthToCrossNext * aOld
-                + newStick.propAlongStickLengthToCrossPrevious * aNew;
+                    + newStick.propAlongStickLengthToCrossPrevious * aNew;
 
                 newStick.y = oldStick.y + oldStick.propAlongStickLengthToCrossNext * bOld
-                + newStick.propAlongStickLengthToCrossPrevious * bNew;
+                    + newStick.propAlongStickLengthToCrossPrevious * bNew;
 
                 var stickTopX = newStick.x - newStick.propAlongStickLengthToCrossNext * aNew;
                 var stickTopY = newStick.y - newStick.propAlongStickLengthToCrossNext * bNew;
@@ -239,10 +259,10 @@ define(['../js/timer.js', 'jquery', '../lib/d3/d3.min.js'], function (timer, $) 
                 // If new stick end exceeds any of the game area bound edges,
                 // set its crossing point for the next stick to be the same as
                 // the point at which it is crossing the stick laid prior to it
-                if ((stickTopY < (gameAreaBounds.top + stickLength))
-                    || (stickTopY > (gameAreaBounds.bottom - stickLength))
-                    || (stickTopX < (gameAreaBounds.left + stickLength))
-                    || (stickTopX > (gameAreaBounds.right - stickLength))) {
+                if ((stickTopY < (gameArea.gameAreaBounds.top + gameArea.stickLength))
+                    || (stickTopY > (gameArea.gameAreaBounds.bottom - gameArea.stickLength))
+                    || (stickTopX < (gameArea.gameAreaBounds.left + gameArea.stickLength))
+                    || (stickTopX > (gameArea.gameAreaBounds.right - gameArea.stickLength))) {
                     newStick.propAlongStickLengthToCrossNext =
                         newStick.propAlongStickLengthToCrossPrevious;
                 }
